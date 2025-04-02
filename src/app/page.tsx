7db29da28929a542a41e68937b51b2bd586cd19f
Patricio <patricio.filice@gmail.com>
2025-04-02 14:46:26 -0300

    initial solution

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <>
+    <header className="flex flex-row justify-between p-4">
+      <h2>Bidify</h2>
+      <div>
+        <p>User</p>
+      </div>
+    </header>
+    <main className="p-4 text-center">
+      <p>In Progress...</p>
+    </main>
+    <footer className="flex flex-row p-4 mt-auto">
+      <p className="ml-auto">Copyright © 2025 Bidify</p>
+    </footer>
+    </>
+  );
+}
